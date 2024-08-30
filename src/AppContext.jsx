5028f68aae47cbefa449b139a844b816d514ecb9
Najam <realnajmiter@gmail.com>
2024-08-30 12:46:19 +0500

    fixed a build issue

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,33 +1,33 @@
-import { createContext, useState, useEffect, useContext } from "react";
-import { supabase } from "./supabaseClient";
-import { useQuery } from "react-query";
-import Skeleton from "./components/Skeleton";
+import { createContext, useState, useEffect, useContext } from 'react';
+import { supabase } from './supabaseClient';
+import { useQuery } from '@tanstack/react-query';
+import Skeleton from './components/Skeleton';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [bio, setBio] = useState("");
-  const [link, setLink] = useState("");
+  const [bio, setBio] = useState('');
+  const [link, setLink] = useState('');
   const [theme, setTheme] = useState(null);
-  const [userName, setUserName] = useState("");
-  const [fullName, setFullName] = useState("");
-  const [profilePic, setProfilePic] = useState("");
-  const [threadText, setThreadText] = useState("");
-  const [postPic, setPostPic] = useState("");
+  const [userName, setUserName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [threadText, setThreadText] = useState('');
+  const [postPic, setPostPic] = useState('');
   const [userPosts, setUserPosts] = useState([]);
 
   useEffect(() => {
-    if (theme === "light") {
-      document.documentElement.classList.remove("dark");
-    } else if (theme === "dark") {
-      document.documentElement.classList.add("dark");
+    if (theme === 'light') {
+      document.documentElement.classList.remove('dark');
+    } else if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
     } else {
       const isDarkMode = window.matchMedia(
-        "(prefers-color-scheme: dark)"
+        '(prefers-color-scheme: dark)'
       ).matches;
       if (isDarkMode) {
-        document.documentElement.classList.add("dark");
+        document.documentElement.classList.add('dark');
       } else {
-        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.remove('dark');
       }
     }
   }, [theme]);
@@ -40,13 +40,13 @@ export const AppProvider = ({ children }) => {
     if (!user) return;
 
     const { data, error } = await supabase
-      .from("usersDetails")
-      .select("*")
-      .eq("user_id", user.id)
+      .from('usersDetails')
+      .select('*')
+      .eq('user_id', user.id)
       .single();
 
     if (error) {
-      console.error("Error fetching user details:", error.message);
+      console.error('Error fetching user details:', error.message);
     } else {
       setUserName(data.user_name);
       setFullName(data.full_name);
@@ -54,11 +54,11 @@ export const AppProvider = ({ children }) => {
       setLink(data.user_link);
 
       const { publicURL, error: urlError } = supabase.storage
-        .from("profile_picture") // Replace with your actual bucket name
+        .from('profile_picture') // Replace with your actual bucket name
         .getPublicUrl(data.profile_url);
 
       if (urlError) {
-        console.error("Error fetching profile picture URL:", urlError.message);
+        console.error('Error fetching profile picture URL:', urlError.message);
       } else {
         setProfilePic(data.profile_url);
       }
@@ -66,7 +66,7 @@ export const AppProvider = ({ children }) => {
     return data;
   };
 
-  const { data, isLoading, error } = useQuery("userDetails", fetchUserDetails, {
+  const { data, isLoading, error } = useQuery('userDetails', fetchUserDetails, {
     staleTime: 5000,
   });
 
@@ -78,12 +78,12 @@ export const AppProvider = ({ children }) => {
     if (!user) return;
 
     const { data, error } = await supabase
-      .from("posts")
-      .select("post_text, post_image")
-      .eq("user_id", user.id);
+      .from('posts')
+      .select('post_text, post_image')
+      .eq('user_id', user.id);
 
     if (error) {
-      console.error("Error fetching user posts:", error.message);
+      console.error('Error fetching user posts:', error.message);
     } else {
       setUserPosts(data);
     }
@@ -94,7 +94,7 @@ export const AppProvider = ({ children }) => {
     data: postsData,
     error: postError,
     isLoading: postLoading,
-  } = useQuery("posts", fetchUserPosts);
+  } = useQuery('posts', fetchUserPosts);
 
   // if (isLoading)
   //   return (
@@ -135,7 +135,7 @@ export const AppProvider = ({ children }) => {
 export function useApp() {
   const data = useContext(AppContext);
   if (!data) {
-    throw new Error("chin tapak dam dam");
+    throw new Error('chin tapak dam dam');
   }
   return data;
 }
